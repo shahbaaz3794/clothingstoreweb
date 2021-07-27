0839f2b689309f565827e99e828bcae6d8c10942
shahbaaz3794 <shahbaaz.ahmad3794@gmail.com>
2021-07-27 23:05:54 +0530

    home categories

--- src/components/homeCategories.js
+++ src/components/homeCategories.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from "react-redux";
+
+const useStyles = makeStyles((theme) => ({
+  
+}));
 
 const HomeCategories = () => {
+  const classes = useStyles();
+
   return (
     <>
       
--- src/components/homeContent.js
+++ src/components/homeContent.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from "react-redux";
+
+const useStyles = makeStyles((theme) => ({
+  
+}));
 
 const HomeContent=()=> {
+  const classes = useStyles();
+
   return (
     <>
       
